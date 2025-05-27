fc89713f2d0cc299174b5089d1a4b4fec99770db
MingTori <s090ldbread@gmail.com>
2025-05-27 21:26:06 +0900

    feat : Swagger 설정 추가

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="SW_2025 API",
+        default_version='v1',
+        description="SSWU 프로젝트 API 문서",
+        contact=openapi.Contact(email="your@email.com"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/users/', include('users.urls')),
+    path('api/', include('users.urls')), 
+
+    # Swagger UI
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
