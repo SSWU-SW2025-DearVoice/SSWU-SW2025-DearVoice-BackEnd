af10cce4c046d4347ab22bd72c715a3cf7f44d41
lynn0325 <hynn8260@gmail.com>
2025-07-06 14:39:21 +0900

    Delete Downloads/SSWU-SW2025-DearVoice-BackEnd-main (2)/SSWU-SW2025-DearVoice-BackEnd-main/letters directory

--- Downloads/SSWU-SW2025-DearVoice-BackEnd-main (2)/SSWU-SW2025-DearVoice-BackEnd-main/letters/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import LetterCreateView, LetterListView, LetterDetailView
-
-
-urlpatterns = [
-    path('create/', LetterCreateView.as_view(), name='letter-create'),
-    path('list/', LetterListView.as_view(), name='letter-list'),
-    path('<int:pk>/', LetterDetailView.as_view(), name='letter-detail'),
-]
