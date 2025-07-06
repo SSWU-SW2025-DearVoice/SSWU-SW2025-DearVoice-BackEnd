0ef6e4b8dfdb60b3c2c1b7150c9328a8e940e3e6
MingTori <s090ldbread@gmail.com>
2025-07-06 13:37:34 +0900

    하늘편지 답신

--- skyvoice/urls.py
+++ skyvoice/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SkyVoiceLetterCreateView
+from .views import SkyVoiceLetterCreateView, SkyVoiceLetterReplyView
 
 urlpatterns = [
     path('create/', SkyVoiceLetterCreateView.as_view(), name='skyvoice-create'),
+    path('reply/ai/<int:pk>/', SkyVoiceLetterAIReplyView.as_view(), name='skyvoice-ai-reply'),
 ]
