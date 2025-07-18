e6c5f621933e8341956f8841ee9a870f4a9cc93b
lynn <164450625+lynn0325@users.noreply.github.com>
2025-07-18 11:49:28 +0900

    Add files via upload

--- letters/urls.py
+++ letters/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import LetterCreateView, LetterListView, LetterDetailView
+from .views import LetterCreateView, LetterListView, LetterDetailView, MyPageLettersView
 
 
 urlpatterns = [
     path('create/', LetterCreateView.as_view(), name='letter-create'),
     path('list/', LetterListView.as_view(), name='letter-list'),
     path('<int:pk>/', LetterDetailView.as_view(), name='letter-detail'),
+     path("mypage/", MyPageLettersView.as_view()),
 ]
